c1890cf9b8e3fa91752ff56bab0b522837a91f08
hiroki19990625 <hiroki19990625@gmail.com>
2021-07-19 01:54:29 +0900

    Format Code

--- MineUtil/Functional.cs
+++ MineUtil/Functional.cs
@@ -4,21 +4,34 @@ namespace MineUtil
 {
     public static class Functional
     {
-        static public T Id<T>(T t) => t;
+        public static T Id<T>(T t)
+        {
+            return t;
+        }
 
-        static public Func<T2, T1, R> Flip<R, T1, T2>(this Func<T1, T2, R> func)
-            => (t2, t1) => func(t1, t2);
+        public static Func<T2, T1, R> Flip<R, T1, T2>(this Func<T1, T2, R> func)
+        {
+            return (t2, t1) => func(t1, t2);
+        }
 
-        static public Action<T2, T1> Flip<T1, T2>(this Action<T1, T2> func)
-            => (t2, t1) => func(t1, t2);
+        public static Action<T2, T1> Flip<T1, T2>(this Action<T1, T2> func)
+        {
+            return (t2, t1) => func(t1, t2);
+        }
 
-        static public Func<T1, T3> Merge<T1, T2, T3>(this Func<T1, T2> func1, Func<T2, T3> func2)
-            => t1 => func2(func1(t1));
+        public static Func<T1, T3> Merge<T1, T2, T3>(this Func<T1, T2> func1, Func<T2, T3> func2)
+        {
+            return t1 => func2(func1(t1));
+        }
 
-        static public Func<T1, Func<T2, R>> Curry<T1, T2, R>(this Func<T1, T2, R> func)
-            => t1 => t2 => func(t1, t2);
+        public static Func<T1, Func<T2, R>> Curry<T1, T2, R>(this Func<T1, T2, R> func)
+        {
+            return t1 => t2 => func(t1, t2);
+        }
 
-        static public Func<T1, Func<T2, Func<T3, R>>> Curry<T1, T2, T3, R>(this Func<T1, T2, T3, R> func)
-            => t1 => t2 => t3 => func(t1, t2, t3);
+        public static Func<T1, Func<T2, Func<T3, R>>> Curry<T1, T2, T3, R>(this Func<T1, T2, T3, R> func)
+        {
+            return t1 => t2 => t3 => func(t1, t2, t3);
+        }
     }
 }
